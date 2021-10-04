13ea38da65f8fc57779d2bb69d53c9e806d71874
NSergeiV <ttniseva@yandex.ru>
2021-10-04 21:34:25 +0300

    Выполнил задание - 2.11. Начинаем программировать

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  FILM_CARD_COUNT: 20,
+  FILM_CARD_TITLE: 'The Grand Budapest Hotel',
+  FILM_CARD_GENRE: 'Drama',
+  FILM_CARD_YEAN: 2014,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      filmCard={Setting}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
