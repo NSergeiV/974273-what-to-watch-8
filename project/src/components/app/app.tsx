d29c4d027a0ca81b6e2d0ff26f5cb53b3221b40e
NSergeiV <ttniseva@yandex.ru>
2021-10-11 00:29:25 +0300

    Выполнил задание - 3.5. Маршрут по тайным тропам

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,13 @@
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import Main from '../main/main';
+import {AppFilms, AuthorizationStatus} from '../../const';
+import ErrorScreen from '../error-screen/error-screen';
+import SignIn from '../sign-in/sign-in';
+import MyList from '../my-list/my-list';
+import Film from '../films/film';
+import AddReview from '../add-review/add-review';
+import Player from '../player/player';
+import PrivateRoute from '../private-route/private-route';
 
 type AppScreenProps = {
   filmCard: {
@@ -10,7 +19,35 @@ type AppScreenProps = {
 }
 
 function App({filmCard}: AppScreenProps): JSX.Element {
-  return <Main filmCard={filmCard} />;
+  // return <Main filmCard={filmCard} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path={AppFilms.Main} exact>
+          <Main filmCard={filmCard} />
+        </Route>
+        <Route path={AppFilms.SignIn} exact>
+          <SignIn />
+        </Route>
+        <Route path={AppFilms.Film} exact>
+          <Film />
+        </Route>
+        <Route path={AppFilms.AddReview} exact>
+          <AddReview />
+        </Route>
+        <Route path={AppFilms.Player} exact>
+          <Player />
+          <AddReview />
+        </Route>
+        <PrivateRoute path={AppFilms.MyList} exact render={() => <MyList />} authorizationStatus={AuthorizationStatus.NoAuth}>
+          <MyList />
+        </PrivateRoute>
+        <Route>
+          <ErrorScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ project/src/const.ts
@@ -0,0 +1,15 @@
+export enum AppFilms {
+  SignIn = '/login',
+  MyList = '/mylist',
+  Film = '/films/:id',
+  Main = '/',
+  AddReview = '/films/:id/review',
+  Player = '/player/:id'
+}
+
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
