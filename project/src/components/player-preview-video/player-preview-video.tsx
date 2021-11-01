2144ea654ec5184cf36ddbcea7abe855c63ededf
NSergeiV <ttniseva@yandex.ru>
2021-11-01 20:40:52 +0300

    Выполнил задаение - 5.13. Больше подробностей (часть 1). Требуется проверка.

--- project/src/components/player-preview-video/player-preview-video.tsx
+++ project/src/components/player-preview-video/player-preview-video.tsx
@@ -1,19 +1,13 @@
-// События мыши - https://ru.reactjs.org/docs/events.html#mouse-events
 import React from 'react';
-import {Fragment} from 'react';
-import {DataFilm} from '../../types/movie-data';
 
-// import {withActiveFlag} from "./withActiveFlag.jsx"; // https://up.htmlacademy.ru/profession/react/7/react/demos/5927#12
+function PlayerPreviewVideo(restProps: any): JSX.Element {
 
-function PlayerPreviewVideo(props: DataFilm): JSX.Element {
-  // const {isActive, onActiveChange, ...restProps} = props;
-  const isActive = true;
+  const {...props} = restProps;
+
+  const prop = props;
 
   return (
-    <Fragment>
-      <div></div>
-      {isActive && <video controls autoPlay src={props.previewVideoLink} width="280" height="175" muted /> || <div className="small-film-card__image"><img src={props.previewImage} alt={props.name} width="280" height="175" /></div>}
-    </Fragment>
+    <video autoPlay src={prop.previewVideoLink} width="280" height="175" muted />
   );
 }
 
